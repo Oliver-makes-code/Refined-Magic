e8499994533543df63890197431eb977ca79d831
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-05-11 21:50:30 -0500

    Packets

--- src/main/java/olivermakesco/de/refmagic/Mod.java
+++ src/main/java/olivermakesco/de/refmagic/Mod.java
@@ -2,12 +2,19 @@ package olivermakesco.de.refmagic;
 
 import com.google.gson.Gson;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import olivermakesco.de.refmagic.augment.AugmentLoader;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import net.minecraft.util.Identifier;
 import olivermakesco.de.refmagic.registry.RefinedMagicRegistry;
+import org.quiltmc.qsl.networking.api.PacketByteBufs;
+import org.quiltmc.qsl.networking.api.PacketSender;
+import org.quiltmc.qsl.networking.api.ServerPlayConnectionEvents;
+import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
 import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
+import org.quiltmc.qsl.resource.loader.api.ResourceLoaderEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,19 +26,44 @@ public class Mod implements ModInitializer {
 	public void onInitialize(ModContainer mod) {
 		RefinedMagicRegistry.register();
 		ResourceLoader.get(ResourceType.SERVER_DATA).registerReloader(new AugmentLoader(new Gson(), "augments"));
+		ResourceLoaderEvents.END_DATA_PACK_RELOAD.register((server, resourceManager, error) -> {
+			if (server == null) return;
+			var start = true;
+			for (var entry : AugmentLoader.augments.entrySet()) {
+				var id = entry.getKey();
+				var augment = entry.getValue();
+				var buffer = PacketByteBufs.create();
+				buffer.writeBoolean(start);
+				if (start) start = false;
+				buffer.writeIdentifier(id);
+				buffer.writeInt(augment.color());
+				buffer.writeInt(augment.potion().size());
+				for (Identifier potion : augment.potion())
+					buffer.writeIdentifier(potion);
+				var players = server.getPlayerManager().getPlayerList();
+				for (var player : players)
+					ServerPlayNetworking.send(player, id("augments"), buffer);
+			}
+		});
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			var start = true;
+			for (var entry : AugmentLoader.augments.entrySet()) {
+				var id = entry.getKey();
+				var augment = entry.getValue();
+				var buffer = PacketByteBufs.create();
+				buffer.writeBoolean(start);
+				if (start) start = false;
+				buffer.writeIdentifier(id);
+				buffer.writeInt(augment.color());
+				buffer.writeInt(augment.potion().size());
+				for (Identifier potion : augment.potion())
+					buffer.writeIdentifier(potion);
+				ServerPlayNetworking.send(handler.player, id("augments"), buffer);
+			}
+		});
 	}
 
 	public static Identifier id(String name) {
 		return new Identifier(modid,name);
-	}
-
-	public static void info(String s) {
-		logger.info(s);
-	}
-	public static void error(String s) {
-		logger.error(s);
-	}
-	public static void debug(String s) {
-		logger.debug(s);
 	}
 }
--- src/main/java/olivermakesco/de/refmagic/ModClient.java
+++ src/main/java/olivermakesco/de/refmagic/ModClient.java
@@ -2,14 +2,25 @@ package olivermakesco.de.refmagic;
 
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
+import olivermakesco.de.refmagic.augment.Augment;
+import olivermakesco.de.refmagic.augment.AugmentLoader;
 import olivermakesco.de.refmagic.client.AltarTableBlockEntityRenderer;
 import olivermakesco.de.refmagic.item.NecklaceItem;
 import olivermakesco.de.refmagic.registry.RefinedMagicBlockEntities;
 import olivermakesco.de.refmagic.registry.RefinedMagicItems;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.networking.api.PacketSender;
+import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ModClient implements ClientModInitializer {
     @Override
@@ -17,7 +28,18 @@ public class ModClient implements ClientModInitializer {
         BlockEntityRendererRegistry.register(RefinedMagicBlockEntities.altarTableBlockEntity, AltarTableBlockEntityRenderer::new);
         ColorProviderRegistry.ITEM.register(new Provider(), RefinedMagicItems.baseNecklace);
 
-
+        ClientPlayNetworking.registerGlobalReceiver(Mod.id("augments"), (client, handler, buf, responseSender) -> {
+            if (client.isInSingleplayer()) return;
+            if (buf.readBoolean())
+                AugmentLoader.augments = new HashMap<>();
+            var id = buf.readIdentifier();
+            var color = buf.readInt();
+            var potionLength = buf.readInt();
+            var potions = new ArrayList<Identifier>();
+            for (int i = 0; i < potionLength; i++)
+                potions.add(buf.readIdentifier());
+            AugmentLoader.augments.put(id, new Augment(color, potions));
+        });
     }
     static class Provider implements ItemColorProvider {
 
