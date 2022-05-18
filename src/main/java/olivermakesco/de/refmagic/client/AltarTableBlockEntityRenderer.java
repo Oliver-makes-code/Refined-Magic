29c8328dacea73f3e82e66e50af06909f520c1c0
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-05-17 19:54:21 -0500

    Abstractify altar rendering

--- src/main/java/olivermakesco/de/refmagic/client/AltarTableBlockEntityRenderer.java
+++ src/main/java/olivermakesco/de/refmagic/client/AltarTableBlockEntityRenderer.java
@@ -15,35 +15,23 @@ public class AltarTableBlockEntityRenderer implements BlockEntityRenderer<AltarT
     public AltarTableBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
 
     }
-
     @Override
     public void render(AltarTableBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         var time = ++rotation;
         double offset = Math.sin((time + tickDelta) / 8.0) / 8.0;
 
-        matrices.push();
-        matrices.translate(0.25, 1.1 + offset, 0.25);
-        drawItem(entity, tickDelta, matrices, vertexConsumers, light, overlay, time,0);
-        matrices.pop();
-        matrices.push();
-        matrices.translate(0.75, 1.1 + offset, 0.25);
-        drawItem(entity, tickDelta, matrices, vertexConsumers, light, overlay, time,1);
-        matrices.pop();
-        matrices.push();
-        matrices.translate(0.25, 1.1 + offset, 0.75);
-        drawItem(entity, tickDelta, matrices, vertexConsumers, light, overlay, time,2);
-        matrices.pop();
-        matrices.push();
-        matrices.translate(0.75, 1.1 + offset, 0.75);
-        drawItem(entity, tickDelta, matrices, vertexConsumers, light, overlay, time,3);
-        matrices.pop();
+        drawItem(0.25, 0.25, offset, entity, tickDelta, matrices, vertexConsumers, light, overlay, time,0);
+        drawItem(0.75, 0.25, offset, entity, tickDelta, matrices, vertexConsumers, light, overlay, time,1);
+        drawItem(0.25, 0.75, offset, entity, tickDelta, matrices, vertexConsumers, light, overlay, time,2);
+        drawItem(0.75, 0.75, offset, entity, tickDelta, matrices, vertexConsumers, light, overlay, time,3);
     }
 
-    private void drawItem(AltarTableBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, long time, int slot) {
+    private void drawItem(double offx, double offz, double offset, AltarTableBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, long time, int slot) {
+        matrices.push();
+        matrices.translate(offx, 1.1 + offset, offz);
         matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion((time + tickDelta) * 4));
         matrices.scale(0.5f,0.5f,0.5f);
         MinecraftClient.getInstance().getItemRenderer().renderItem(entity.getSlot(slot), ModelTransformation.Mode.FIXED, light, overlay, matrices, vertexConsumers, 0);
         matrices.pop();
-        matrices.push();
     }
 }
